d6ca82eac7fa3c02e6bd9f3fb95fd35225585400
Maksim <merlinkory@yandex.ru>
2022-02-23 19:57:50 +0700

    refactoring,  add offersByAddress, add removeFromSale

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -5,27 +5,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MarketPlace{
 
-    event offerCreated(bytes32 indexed offerId, address indexed hostContract, address indexed offerer,  uint tokenId, uint price, string uri);
-    event OfferClosed(bytes32 indexed offerId, address indexed buyer);
+    event offerCreated(uint indexed offerId, address indexed hostContract, address indexed offerer,  uint tokenId, uint price, string uri);
+    event OfferClosed(uint indexed offerId, address indexed buyer);
     event BalanceWithdrawn (address indexed beneficiary, uint amount);
     
 
 
-    uint public offeringNonce;
+    uint public offeringNonce = 1;
 
     struct offering {
-        address offerer;
+        address owner;
         address hostContract;
         uint tokenId;
         string uri;
         uint price;
-        bool closed; 
+        bool active; 
     }
     
-    mapping (bytes32 => offering) public offeringRegistry;
+    // mapping (bytes32 => offering) public offeringRegistry;
+    offering[] public offers;
+    
     mapping (address => uint) public balances;
+    mapping (address => uint) public tokenBalances;
+
     uint public companyBalance ;
-    bytes32[] public offersids;
+    
 
     uint public fee = 1; // 1% commision for each deal;
 
@@ -41,6 +45,26 @@ contract MarketPlace{
 
     }
 
+    function offersByAddress(address _address, bool _active) external view returns(uint[] memory){
+        uint[] memory results = new uint[](tokenBalances[_address]);
+        uint counter = 0;
+        for(uint i = 0; i<=offers.length; i++){
+            if(offers[i].owner == _address && offers[i].active == _active){
+                results[counter] = i;
+                counter++;
+            }
+        }
+
+        return results;
+    }
+
+    function removeFromSale(uint _offerId) external{
+        // offers[_offerId].active == false;
+        require(msg.sender == offers[_offerId].owner, "Only owner can delete offer");
+        delete offers[_offerId];
+
+    }
+
     function createOffer(address _hostContract, uint _tokenId, uint _price) external {
 
         ERC721 hostContract = ERC721(_hostContract);        
@@ -50,29 +74,22 @@ contract MarketPlace{
 
         string memory uri = hostContract.tokenURI(_tokenId);
 
-        bytes32 offerId = keccak256(abi.encodePacked(offeringNonce, _hostContract, _tokenId));
-        offersids.push(offerId);
-        offeringRegistry[offerId].offerer = msg.sender;
-        offeringRegistry[offerId].hostContract = _hostContract;
-        offeringRegistry[offerId].tokenId = _tokenId;
-        offeringRegistry[offerId].uri = uri;
-        offeringRegistry[offerId].price = _price;
-        offeringNonce += 1;
-        
-        
-        emit  offerCreated(offerId, _hostContract, msg.sender, _tokenId, _price, uri);
+        offers.push(offering(msg.sender,_hostContract,_tokenId,uri,_price,true));
+
+        tokenBalances[msg.sender]++;                   
+        emit  offerCreated(offers.length, _hostContract, msg.sender, _tokenId, _price, uri);
     }
     
-    function closeOffer(bytes32 _offerId) external payable {
-        require(msg.value >= offeringRegistry[_offerId].price, "Not enough funds to buy");
-        require(offeringRegistry[_offerId].closed != true, "Offering is closed");
-        ERC721 hostContract = ERC721(offeringRegistry[_offerId].hostContract);
-        hostContract.safeTransferFrom(offeringRegistry[_offerId].offerer, msg.sender, offeringRegistry[_offerId].tokenId);
-        offeringRegistry[_offerId].closed = true;
+    function closeOffer(uint _offerId) external payable {
+        require(msg.value >= offers[_offerId].price, "Not enough funds to buy");
+        require(offers[_offerId].active != false, "Offering is closed");
+        ERC721 hostContract = ERC721(offers[_offerId].hostContract);
+        hostContract.safeTransferFrom(offers[_offerId].owner, msg.sender, offers[_offerId].tokenId);
+        offers[_offerId].active = false;
 
         uint commision = msg.value * fee / 100;
 
-        balances[offeringRegistry[_offerId].offerer] += (msg.value - commision);
+        balances[offers[_offerId].owner] += (msg.value - commision);
         companyBalance += commision;
         emit OfferClosed(_offerId, msg.sender);
     } 
