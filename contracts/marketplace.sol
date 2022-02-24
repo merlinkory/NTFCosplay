dc3c2817010ad2829c8868908ba330772d6b3b2a
Maksim <merlinkory@yandex.ru>
2022-02-24 15:10:33 +0700

    add royalty to creator

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -3,22 +3,24 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+abstract contract cosplayNft is ERC721 {
+    function getNftCreator(uint _tokenId) external view virtual returns(address);
+}
+
 contract MarketPlace{
 
     event offerCreated(uint indexed offerId, address indexed hostContract, address indexed offerer,  uint tokenId, uint price, string uri);
     event OfferClosed(uint indexed offerId, address indexed buyer);
     event BalanceWithdrawn (address indexed beneficiary, uint amount);
     
-
-
-    uint public offeringNonce = 1;
-
     struct offering {
+        address creator;
         address owner;
         address hostContract;
         uint tokenId;
         string uri;
         uint price;
+        uint royaltyAmount;
         bool active; 
     }
     
@@ -26,7 +28,10 @@ contract MarketPlace{
     offering[] public offers;
     
     mapping (address => uint) public balances;
-    mapping (address => uint) public tokenBalances;
+    mapping (address => uint) public offerCountByAddress;
+    mapping (uint =>uint) public royaltyByTokenId;
+
+    uint public offerCount = 0; // total offers in marketplace
 
     uint public companyBalance ;
     
@@ -46,7 +51,7 @@ contract MarketPlace{
     }
 
     function offersByAddress(address _address, bool _active) external view returns(uint[] memory){
-        uint[] memory results = new uint[](tokenBalances[_address]);
+        uint[] memory results = new uint[](offerCountByAddress[_address]);
         uint counter = 0;
         for(uint i = 0; i<offers.length; i++){
             if(offers[i].owner == _address && offers[i].active == _active){
@@ -59,35 +64,60 @@ contract MarketPlace{
     }
 
     function removeFromSale(uint _offerId) external{
-        // offers[_offerId].active == false;
         require(msg.sender == offers[_offerId].owner, "Only owner can delete offer");
         delete offers[_offerId];
 
     }
 
-    function createOffer(address _hostContract, uint _tokenId, uint _price) external {
+    function createOffer(address _hostContract, uint _tokenId, uint _price, uint _royalty) external {
 
-        ERC721 hostContract = ERC721(_hostContract);        
-        address NftOwner = hostContract.ownerOf(_tokenId);
+        cosplayNft hostContract = cosplayNft(_hostContract);        
+        address nftOwner = hostContract.ownerOf(_tokenId);
+        address nftCreator = hostContract.getNftCreator(_tokenId);
+        require (msg.sender == nftOwner, "Only owner of NFT can create offerings");
 
-        require (msg.sender == NftOwner, "Only owner of NFT can create offerings");
+
+        //if creator making offer he can set royalty 
+        if (nftOwner == nftCreator){
+            royaltyByTokenId[_tokenId] = _royalty;
+        }
+
+
+        uint tokenRoyalty = royaltyByTokenId[_tokenId];
+
+        //adding royalty to origin price
+        uint royaltyAmount = 0;
+        if (tokenRoyalty > 0){
+            royaltyAmount = _price * tokenRoyalty / 100;
+            _price = _price + royaltyAmount;
+        }
 
         string memory uri = hostContract.tokenURI(_tokenId);
 
-        offers.push(offering(msg.sender,_hostContract,_tokenId,uri,_price,true));
+        offers.push(offering(nftCreator,msg.sender,_hostContract,_tokenId,uri,_price,royaltyAmount, true));
 
-        tokenBalances[msg.sender]++;                   
+        offerCountByAddress[msg.sender]++;                   
         emit  offerCreated(offers.length, _hostContract, msg.sender, _tokenId, _price, uri);
     }
     
     function closeOffer(uint _offerId) external payable {
-        require(msg.value >= offers[_offerId].price, "Not enough funds to buy");
+        require(msg.sender != offers[_offerId].owner, "Owner cant buy from himself");
+        require(msg.value == offers[_offerId].price, "Not enough funds to buy");
         require(offers[_offerId].active != false, "Offering is closed");
-        ERC721 hostContract = ERC721(offers[_offerId].hostContract);
-        hostContract.safeTransferFrom(offers[_offerId].owner, msg.sender, offers[_offerId].tokenId);
+
+        uint tokenId = offers[_offerId].tokenId;
+      
+
+        cosplayNft hostContract = cosplayNft(offers[_offerId].hostContract);
+        hostContract.safeTransferFrom(offers[_offerId].owner, msg.sender, tokenId);
         offers[_offerId].active = false;
 
-        uint commision = msg.value * fee / 100;
+        //send royalty
+        if(offers[_offerId].royaltyAmount > 0){
+            payable (offers[_offerId].creator).transfer(offers[_offerId].royaltyAmount);
+        }
+
+        uint commision = (msg.value - offers[_offerId].royaltyAmount ) * fee / 100;
 
         balances[offers[_offerId].owner] += (msg.value - commision);
         companyBalance += commision;
